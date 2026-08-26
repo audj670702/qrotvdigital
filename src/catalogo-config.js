4663d1f198f7c1f5e7da1abbe4bca95bc54047cb
SCaD <gestor.isp@gmail.com>
2026-08-26 15:22:19 -0600

    Conecta catálogo de programas con endpoint Wix

--- src/catalogo-config.js
+++ src/catalogo-config.js
@@ -1,4 +1,3 @@
 // TV Digital INTERNET
 // Configuración del Catálogo de Producciones
-// Sustituir con el endpoint Wix publicado para TVD_Catalogo_Programas.
-window.TVD_CATALOGO_ENDPOINT = '';
+window.TVD_CATALOGO_ENDPOINT = 'https://scad.mx/_functions/qtdCatalogoProgramas';
